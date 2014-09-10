9d7d271167e6e787ff84ecc2921785aba714057a
Craig Waite <craigvl@tpg.com.au>
2014-09-10 14:01:55 +1000

    Start of server.

---
+++ Server.js
@@ -0,0 +1,22 @@
+﻿var Hapi = require('hapi');
+var server = new Hapi.Server(3000);
+var http = require('http');
+var requests = require('request');
+var Joi = require('joi');
+
+var user = {
+    username: '',
+    password: ''
+}
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Just Giving!');
+    }
+});
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
