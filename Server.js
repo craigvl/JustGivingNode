9017a0b9a4ed55f74effd38dc88ed1e4b025331f
Craig Waite <craigvl@tpg.com.au>
2014-09-12 09:17:35 +1000

    Updates

--- Server.js
+++ Server.js
@@ -17,7 +17,7 @@ server.route({
     method: 'GET',
     path: '/countries',
     handler: function (request, reply) {
-        requests({ headers: { 'Content-Type': 'application/json' }, url: 'https://api-sandbox.justgiving.com/{API Key}/v1/countries' }, function (error, response, body) {
+        requests({ headers: { 'Accept': 'application/json','Content-Type': 'application/json' }, url: 'https://api-sandbox.justgiving.com/{API Key}/v1/countries' }, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log("Country search called...");
                 reply(JSON.parse(body));
@@ -35,7 +35,7 @@ server.route({
     method: 'GET',
     path: '/charity/categories',
     handler: function (request, reply) {
-        requests({ headers: { 'Content-Type': 'application/json' }, url: 'https://api-sandbox.justgiving.com/{API Key}/v1/charity/categories' }, function (error, response, body) {
+        requests({ headers: { 'Accept': 'application/json','Content-Type': 'application/json' }, url: 'https://api-sandbox.justgiving.com/{API Key}/v1/charity/categories' }, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log("Category search called...");
                 reply(JSON.parse(body));
@@ -48,12 +48,35 @@ server.route({
     }
 });
 
+//http://localhost:3000/searchall/dog
+server.route({
+    method: 'GET',
+    path: '/searchall/{query}',
+    handler: function (request, reply) {
+        requests({ headers: { 'Accept': 'application/json','Content-Type': 'application/json' }, url: 'https://api-sandbox.justgiving.com/{API Key}/v1/onesearch?q=' + request.params.query }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var foo = JSON.parse(body);
+                console.log("All search total records found: "+ foo.Total);
+                for (var index in foo.GroupedResults) {
+                    console.log(foo.GroupedResults[index].Title);
+                }
+                reply(JSON.parse(body));
+            }
+            else {
+                console.log("Error with the all search...");
+                reply("Error");
+            }
+        })
+    }
+});
+
+
 //http://localhost:3000/charity/search/dog
 server.route({
     method: 'GET',
     path: '/charity/search/{query}',
     handler: function (request, reply) {
-        requests({ headers: { 'Content-Type': 'application/json' }, url: 'https://api-sandbox.justgiving.com/{API Key}/v1/charity/search?q=' + request.params.query}, function (error, response, body) {
+        requests({ headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }, url: 'https://api-sandbox.justgiving.com/{API Key}/v1/charity/search?q=' + request.params.query }, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 var foo = JSON.parse(body);
                 console.log('Number of Hits:' + JSON.parse(foo.numberOfHits) + " for query:" + request.params.query);
